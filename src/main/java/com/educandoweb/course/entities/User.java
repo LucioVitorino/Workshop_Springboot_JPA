733c679c7d1ef7bcc6eca0617b2ab5342f505797
Lucio Vitorino <luciovitorino64@gmail.com>
2026-02-20 22:01:17 +0100

    test database H2, JPA

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,76 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private String password;
+    private String telefone;
+
+    public User() {
+    }
+
+    public User(String nome, long id, String email, String password, String telefone) {
+        this.nome = nome;
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.telefone = telefone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll()
+    {
+        User u = new User("Maria", 1L, "marial@gmail.com", "12345", "9999999");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
